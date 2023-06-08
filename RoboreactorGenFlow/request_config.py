40d65a27fc11a7943575d8ee1a35bf9a93e83a98
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-06-08 08:30:37 +0700

    Update request_config.py

--- RoboreactorGenFlow/request_config.py
+++ RoboreactorGenFlow/request_config.py
@@ -73,7 +73,7 @@ class Authentication_function(object):
                 #return logic to the authentication to check the status of the hardware connection 
                 q_res = res.json().get(Authentication_data.get('Email'))
                 token_data = str(q_res[0])+'.'+str(token)
-                decode_Data = jwt.decode(q_res[0]+'.'+str(token),str(q_res[1]) ,algorithm=["HS512"])
+                decode_Data = jwt.decode(q_res[0]+'.'+str(token),str(q_res[1]) ,algorithms=["HS512"])
                 return decode_Data 
 
 def Authentication_system(Email,token,secret,Project): 
